6e2c3378502e7e8c64d5aa80e735d3fbef275fe9
Zain T <zain.tariq@mail.utoronto.ca>
2024-06-11 01:40:00 -0400

    added test on line 19

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -9,27 +9,32 @@ class FizzBuzz {
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            dofizzbuzz(i);
+        }
+    }
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+    private static void dofizzbuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 10 == 0;
+        int zai = 17;
 
-                System.out.println("Fizz Buzz");
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-            } else if (divisibleBy3) {
+            System.out.println("Fizz Buzz");
 
-                System.out.println("Fizz");
+        } else if (divisibleBy3) {
 
-            } else if (divisibleBy5) {
+            System.out.println("Fizz");
 
-                System.out.println("Buzz");
+        } else if (divisibleBy5) {
 
-            } else {
+            System.out.println("Buzz");
 
-                System.out.println(i);
+        } else {
 
-            }
+            System.out.println(i);
+
         }
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class FizzBuzz111 {
+
+    public static void main(String[] args) {
+        int test = 9;
+    }
+}
